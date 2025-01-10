ababff274b9fa81f72cb5db16c8a5ec9afc84ac9
Aude Bruant <audebrnt@gmail.com>
2025-01-11 00:10:42 +0100

    ajout du composant accueil + css ok

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TetiereComponent } from './tetiere/tetiere.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet, TetiereComponent],
+    imports: [RouterOutlet, TetiereComponent, FooterComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css'
 })
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,15 +1,17 @@
 import { Routes } from '@angular/router';
 import { BoutiqueComponent } from './boutique/boutique.component';
 import { PanierComponent } from './panier/panier.component';
+import { AccueilComponent } from './accueil/accueil.component';
 // import { SigninComponent } from './signin/signin.component';
 // import { SignupComponent } from './signup/signup.component';
 // import { UserProfileComponent } from './user-profile/user-profile.component';
 
 export const routes: Routes = [
+    {path : 'accueil' , component : AccueilComponent},
     {path : 'boutique' , component : BoutiqueComponent},
     {path : 'panier', component : PanierComponent},
     // {path : 'connexion', component : SigninComponent},
     // {path : 'inscription', component : SignupComponent},
     // {path : 'profile', component : UserProfileComponent},
-    {path: '**', redirectTo: 'boutique'}
+    {path: '**', redirectTo: 'accueil'}
 ];
