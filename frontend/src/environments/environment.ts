739ccf860eca0197f8c98c370d76ba481d74b49f
Aude Bruant <audebrnt@gmail.com>
2025-01-10 01:03:52 +0100

    ajout des composants signin et signup + ajout dans les routes

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,3 +1,6 @@
 export const environment = {production : false,
-    backend : "/assets/mock/produits.json"
+    backend : "/assets/mock/produits.json",
+    backendUserSignin: 'http://localhost:3000/api/utilisateur/login',
+    backendUserSignup: 'http://localhost:3000/api/utilisateur/register',
 }
+
